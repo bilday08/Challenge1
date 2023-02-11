864c49e95fc8c67cf5a6a002092d24a44559a074
bilday08 <114985986+bilday08@users.noreply.github.com>
2023-02-11 23:53:31 +0700

    commit

--- .history/my-app/src/App_20230211224712.js
+++ .history/my-app/src/App_20230211224712.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginForm from './Components/LoginForm/LoginForm';
 
 function App() {
   return (
---
+++ .history/my-app/src/Components/LoginForm/LoginForm_20230211230336.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [verificationCode, setVerificationCode] = useState('');
+
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+  };
+
+  const handleVerificationCodeChange = (e) => {
+    setVerificationCode(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Phone number:', phoneNumber);
+    console.log('Verification code:', verificationCode);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="phoneNumber">Số điện thoại:</label>
+      <input
+        type="text"
+        id="phoneNumber"
+        value={phoneNumber}
+        onChange={handlePhoneNumberChange}
+      />
+      <br />
+      <label htmlFor="verificationCode">Mã xác thực:</label>
+      <input
+        type="text"
+        id="verificationCode"
+        value={verificationCode}
+        onChange={handleVerificationCodeChange}
+      />
+      <br />
+    </form>
+  );
+};
+
+export default LoginForm;
